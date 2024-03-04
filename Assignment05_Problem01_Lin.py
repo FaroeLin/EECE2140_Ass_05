11e1f89556aca2c41501a27de99f43404cc55d39
Evanchito <ben2000@aliyun.com>
2024-03-04 18:59:34 -0500

    Function that answers the first question contains comments

--- Assignment05_Problem01_Lin.py
+++ Assignment05_Problem01_Lin.py
@@ -1,4 +1,5 @@
 def greetUser(firstName, lastName):
-    fullName = firstName + " " + lastName
-    greeting = f"Hello, {fullName}! How are you today?"
+    fullName = firstName + " " + lastName       #Spell the fullname
+    greeting = f"Hello, {fullName}! How are you today?"     #Print the fullname with greeting
+    
     return greeting
