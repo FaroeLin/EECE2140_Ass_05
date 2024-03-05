bba9fac847697df05ad0cfbc9b6bc00b9878c009
Evanchito <ben2000@aliyun.com>
2024-03-04 19:24:59 -0500

    Function answering the 6th question

---
+++ Assignment05_Problem06_Lin.py
@@ -0,0 +1,4 @@
+def displayCount(start, end):
+
+    for number in range(start, end + 1):        #Adding 1 to ensures the end number is part of the sequence
+        print(number)       #Print each number
